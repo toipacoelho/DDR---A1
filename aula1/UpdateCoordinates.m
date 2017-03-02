4fd650813e868e723b5b3a9043a9b7d9db4e5a3d
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-03-02 15:20:38 +0000

    mask

--- aula1/UpdateCoordinates.m
+++ aula1/UpdateCoordinates.m
@@ -2,6 +2,6 @@ function[pos,vel]= UpdateCoordinates(pos,vel,delta)
     pos = pos + vel*delta;
     vel(pos<0)= -vel(pos<0);
     N = size(pos,1);
-    mask= horzcat(ones(N,1)*300,ones(N,1)*200);
+    mask= [ones(N,1)*300 ones(N,1)*200];
     vel(pos>mask) = -vel(pos>mask);
 end
