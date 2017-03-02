e67987589258686fab5f342b6864454c22b1beb6
toipacoelho <toipacoelho@gmail.com>
2017-03-02 16:53:36 +0000

    labeling working

--- aula1/AverageConnectedNodePairs.m
+++ aula1/AverageConnectedNodePairs.m
@@ -1,11 +1,15 @@
 function o= AverageConnectedNodePairs(N,L)
     labels= 1:N;
-    [m,n] = size(L);
-    for i=1:m
-        labels(L(i,2))=L(i,1);
-    end
-    for i=1:N
-        
-    end
+    r=true;
+    while r
+        labels
+        r=false;
+        for i=1:size(L,1)
+            if labels(L(i,1))~=labels(L(i,2))
+                labels(labels==labels(L(i,2))) = labels(L(i,1));
+                r=true;
+            end
+        end
+     end
     o=1;
 end
