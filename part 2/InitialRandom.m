34715bf3672439d30f205abad2552847a4dae3dd
toipacoelho <Toipacoelho@gmail.com>
2017-03-04 16:40:53 +0000

    added ap nodes

--- part 2/InitialRandom.m
+++ part 2/InitialRandom.m
@@ -1,6 +1,22 @@
-function [pos,vel]= InitialRandom(N,S)
+function [pos,vel, npos]= InitialRandom(N,S,No)
     pos = [300*rand(N,1) 200*rand(N,1)];
     abs_val = S*rand(N,1);
     angle_val= 2*pi*rand(N,1);
     vel= [abs_val.*cos(angle_val) abs_val.*sin(angle_val)];
+    switch No
+        case 1         
+            npos= [150 100];
+        case 2
+            npos= [75 100;
+                225 100];
+        case 3
+            npos= [50 100;
+                150 100;
+                250 100];                
+        case 4 
+            npos= [75 150;
+                225 150;
+                75 50;
+                225 50];
+    end
 end
--- part 2/P2.m
+++ part 2/P2.m
@@ -4,15 +4,19 @@ W= 40;              % Radio range (in meters)
 S= 3;               % Maximum speed (in Km/h)
 delta= 1;           % Difference between consecutive time instants (in seconds)
 T= 3600;            % No. of time instants of the simulation
+No = 4;             % No. of nodes
 
 S= S/3.6;           % Conversion of maximum speed to m/s
 results= zeros(1,T);% Initialization of the results array
 
 % Generation of initial coordinates, speed and direction of mobile nodes:
-[pos,vel]= InitialRandom(N,S);
+[pos,vel, npos]= InitialRandom(N,S,No);
 % Visualize node positions:
 figure(1)
+hold on
 plot(pos(:,1),pos(:,2),'o','MarkerEdgeColor','b','MarkerFaceColor','b')
+plot(npos(:,1),npos(:,2),'s','MarkerEdgeColor','b','MarkerFaceColor','b','MarkerSize',15)
+hold off
 axis([0 300 0 200])
 drawnow;
 % Simulation cycle running all time instants iter:
@@ -25,7 +29,10 @@ for iter= 1:T
     % Update node coordinates and speed values:
     [pos,vel]= UpdateCoordinates(pos,vel,delta);
     % Visualize updated node positions:
+    hold on
     plot(pos(:,1),pos(:,2),'o','MarkerEdgeColor','b','MarkerFaceColor','b')
+    plot(npos(:,1),npos(:,2),'s','MarkerEdgeColor','b','MarkerFaceColor','b','MarkerSize',15)
+    hold off
     axis([0 300 0 200])
     drawnow;
 end
